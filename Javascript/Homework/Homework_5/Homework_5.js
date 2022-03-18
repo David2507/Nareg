d808cb646865e5ca7aacc5cf1d33910cf82aa01e
David2507 <davit.minasyan.0@gmail.com>
2022-03-18 17:13:13 +0400

    added homework

---
+++ Javascript/Homework/Homework_5/Homework_5.js
@@ -0,0 +1,43 @@
+console.log("Exercise LIN107 :");
+let x = +prompt("Enter number");
+console.log(++x, ++x, ++x);
+
+
+console.log("Exercise LIN108 :");
+console.log(x + 3, x + 2, x + 1);
+
+
+console.log("Exercise LIN109 :");
+console.log(x - 3, x - 2, x - 1);
+
+
+console.log("Exercise LIN110 :");
+console.log(--x, --x, --x);
+
+
+console.log("Exercise LINA0002 :");
+let y = +prompt("Enter number");
+console.log(y%10);
+alert(y%10);
+
+
+console.log("Exercise LINA0003 :");
+console.log(parseInt(y / 10 % 10));
+
+
+console.log("Exercise LINA0004 :");
+let num = +prompt("Enter first number");
+let num1 = +prompt("Enter second number");
+console.log(Math.round(num * 10) / 10);
+console.log(Math.round(num1 * 10) / 10);
+
+
+
+console.log("Exercise LINA0005 :");
+let man = +prompt("Enter number of mandarins");
+console.log(man * 10);
+
+
+console.log("Exercise LINA0006 :");
+let k = +prompt("Enter number of lessons");
+console.log(5 * (k - 1));
