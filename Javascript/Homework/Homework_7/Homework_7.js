60486e16259c6e35ff6c734c47d7dfd112c72050
David2507 <davit.minasyan.0@gmail.com>
2022-03-30 12:58:46 +0400

    added homework Nareg

---
+++ Javascript/Homework/Homework_7/Homework_7.js
@@ -0,0 +1,905 @@
+console.log("Exercise EASY0301 :");
+let x = 5;
+let st5 = "";
+for (let i = 0; i < x; i++) {
+    st5 = st5 + 1;
+}
+console.log(st5);
+
+
+console.log("Exercise EASY0302 :");
+let st6 = "";
+for (let i = 0; i < x; i++) {
+    st6 = st6 + 1 + " ";
+}
+console.log(st6);
+
+
+console.log("Exercise EASY0303 :");
+for (let i = 0; i < x; i++) {
+    console.log(1);
+
+}
+
+console.log("Exercise EASY0304 :");
+let st4 = "";
+for (let i = 0; i < x; i++) {
+    st4 = st4 + "*";
+}
+console.log(st4);
+
+console.log("Exercise EASY0305 :");
+let st3 = "";
+for (let i = 0; i < x; i++) {
+    st3 = st3 + "*" + " ";
+}
+console.log(st3);
+
+
+console.log("Exercise EASY0306 :");
+for (let i = 0; i < x; i++) {
+    console.log("*");
+
+}
+
+
+console.log("Exercise EASY0307 :");
+let st7 = "";
+for (let i = 0; i < x; i++) {
+    st7 = st7 + "+-";
+}
+console.log(st7);
+
+
+console.log("Exercise EASY0308 :");
+for (let i = 0; i < x; i++) {
+    console.log("+-");
+
+}
+
+
+console.log("Exercise EASY0309 :");
+let st8 = "";
+for (let i = 0; i < x; i++) {
+    st8 = st8 + x + " ";
+}
+console.log(st8);
+
+
+console.log("Exercise EASY0310 :");
+for (let i = 0; i < x; i++) {
+    console.log(x);
+
+}
+
+
+console.log("Exercise EASY0311 :");
+let st9 = "";
+for (let i = 1; i <= x; i++) {
+    st9 = st9 + i + " ";
+}
+console.log(st9);
+
+console.log("Exercise EASY0312 :");
+for (let i = 1; i <= x; i++) {
+    console.log(i);
+}
+
+
+console.log("Exercise EASY0313 :");
+let str3 = "";
+for (let i = 1; i <= x; i++) {
+    str3 = str3 + i + i + " ";
+}
+console.log(str3);
+
+
+console.log("Exercise EASY0314 :");
+for (let i = 1; i <= x; i++) {
+    console.log(i + "" + i);
+}
+
+
+console.log("Exercise EASY0315 :");
+let st = "";
+for (let i = 1; i <= x; i++) {
+    st = st + i + " " + i + " ";
+}
+console.log(st);
+
+console.log("Exercise EASY0316 :");
+for (let i = 1; i <= x; i++) {
+    console.log(i, i);
+}
+
+console.log("Exercise EASY0317 :");
+let str4 = "";
+let a1 = 12;
+for (let i = 1; i <= x; i++) {
+    str4 = str4 + a1 + " ";
+    a1 += 11;
+}
+console.log(str4);
+
+
+console.log("Exercise EASY0318 :");
+let a = 12;
+for (let i = 0; i < x; i++) {
+    console.log(a);
+    a += 11;
+}
+
+
+console.log("Exercise EASY0321 :");
+let str5 = "";
+let a2 = 121;
+for (let i = 1; i <= x; i++) {
+    str5 = str5 + a2 + " ";
+    a2 += 111;
+}
+console.log(str5);
+
+
+console.log("Exercise EASY0322 :");
+let b = 121;
+for (let i = 0; i < x; i++) {
+    console.log(b);
+    b += 111;
+}
+
+
+console.log("Exercise EASY0325 :");
+let st10 = "";
+for (let i = 1; i <= x; i++) {
+    st10 = st10 + i + " ";
+}
+console.log(st10 + "" + st10);
+
+
+console.log("Exercise EASY0326 :");
+let str6 = "";
+for (let i = x; i > 0; i--) {
+    str6 = str6 + i + " ";
+}
+console.log(str6);
+
+console.log("Exercise EASY0327 :");
+for (let i = x; i > 0; i--) {
+    console.log(i);
+}
+
+
+console.log("Exercise EASY0328 :");
+let st11 = "";
+let st21 = "";
+let c1 = 1;
+for (let i = x; i > 0; i--, c1++) {
+    st11 = st11 + " " + i;
+    st21 = st21 + " " + c1;
+}
+console.log(st21 + "" + st11);
+
+
+console.log("Exercise EASY0329 :");
+let st12 = "";
+let st22 = "";
+let c2 = 1;
+for (let i = x; i > 0; i--, c2++) {
+    st11 = st12 + " " + i;
+    st21 = st22 + " " + c2
+}
+console.log(st12 + "" + st22);
+
+
+console.log("Exercise EASY0330 :");
+let st1 = "";
+let st2 = "";
+let c = 1;
+for (let i = x; i > 0; i--, c++) {
+    st1 = st1 + " " + i;
+    st2 = st2 + " " + c;
+}
+console.log(st2);
+console.log(st1);
+
+console.log("Exercise EASY0331 :");
+let st13 = "";
+let st23 = "";
+let c3 = 1;
+for (let i = x; i > 0; i--, c3++) {
+    st13 = st13 + " " + i;
+    st23 = st23 + " " + c3;
+}
+console.log(st13);
+console.log(st23);
+
+
+console.log("Exercise EASY0332 :");
+let st44 = "";
+let d1 = 1;
+for (let i = x; i > 0; i--) {
+    st44 = st44 + d1 + " " + i + " ";
+    d1++;
+}
+console.log(st44);
+
+
+console.log("Exercise EASY0333 :");
+let st43 = "";
+let d2 = 1;
+for (let i = x; i > 0; i--) {
+    st43 = st43 + i + " " + d2 + " ";
+    d2++;
+}
+console.log(st43);
+
+
+console.log("Exercise EASY0334 :");
+let d3 = 1;
+for (let i = x; i > 0; i--) {
+    console.log(d3, i);
+    d3++;
+}
+
+
+console.log("Exercise EASY0335 :");
+let d = 1;
+for (let i = x; i > 0; i--) {
+    console.log(i, d);
+    d++;
+}
+
+
+console.log("Exercise EASY0336 :");
+let d4 = 1;
+for (let i = x; i > 0; i--) {
+    console.log(d4, i, d4);
+    d4++;
+}
+
+console.log("Exercise EASY0337 :");
+let d5 = 1;
+for (let i = x; i > 0; i--) {
+    console.log(i, d5, i);
+    d5++;
+}
+
+
+console.log("Exercise EASY0338 :");
+let d6 = 1;
+for (let i = x; i > 0; i--) {
+    console.log(d6, i, i, d6);
+    d6++;
+}
+
+console.log("Exercise EASY0339 :");
+let d7 = 1;
+for (let i = x; i > 0; i--) {
+    console.log(i, d7, d7, i);
+    d7++;
+}
+console.log("Exercise EASY0340 :");
+let str25 = "";
+for (let i = 1; i <= x; i++) {
+    str25 = str25 + i + " ";
+}
+str188 = " ";
+for (let j = 0; j <= x; j++) {
+    str188 = str188 + " ";
+}
+console.log(str25);
+console.log(str188 + str25);
+
+
+console.log("Exercise EASY0341 :");
+let str14 = "";
+let str24 = "";
+let e4 = 1;
+for (let i = x; i > 0; i--, e4++) {
+    str14 = str14 + i + " ";
+    str24 = str24 + e4 + " ";
+}
+str189 = " ";
+for (let j = 0; j <= x; j++) {
+    str189 = str189 + " ";
+}
+console.log(str24);
+console.log(str189 + str14);
+
+console.log("Exercise EASY0342 :");
+let str13 = "";
+let str23 = "";
+let e3 = 1;
+for (let i = x; i > 0; i--, e3++) {
+    str13 = str13 + i + " ";
+    str23 = str23 + e3 + " ";
+}
+str1891 = " ";
+for (let j = 0; j <= x; j++) {
+    str1891 = str1891 + " ";
+}
+console.log(str13);
+console.log(str1891 + str23);
+
+console.log("Exercise EASY0343 :");
+let str26 = "";
+for (let i = 1; i <= x; i++) {
+    str26 = str26 + i + " ";
+}
+str1892 = " ";
+for (let j = 0; j <= x; j++) {
+    str1892 = str1892 + " ";
+}
+console.log(str26);
+console.log(str1892 + str26);
+console.log(str26);
+
+
+console.log("Exercise EASY0344 :");
+let str17 = "";
+let str27 = "";
+let e7 = 1;
+for (let i = x; i > 0; i--, e7++) {
+    str17 = str17 + i + " ";
+    str27 = str27 + e7 + " ";
+}
+str1893 = " ";
+for (let j = 0; j <= x; j++) {
+    str1893 = str1893 + " ";
+}
+console.log(str27);
+console.log(str1893 + str17);
+console.log(str27);
+
+console.log("Exercise EASY0345:");
+let str12 = "";
+let str22 = "";
+let e2 = 1;
+for (let i = x; i > 0; i--, e2++) {
+    str12 = str12 + i + " ";
+    str22 = str22 + e2 + " ";
+}
+str1894 = " ";
+for (let j = 0; j <= x; j++) {
+    str1894 = str1894 + " ";
+}
+console.log(str12);
+console.log(str1894 + str22);
+console.log(str12);
+
+console.log("Exercise EASY0346 :");
+let str28 = "";
+for (let i = 1; i <= x; i++) {
+    str28 = str28 + i + " ";
+}
+str1895 = " ";
+for (let j = 0; j <= x; j++) {
+    str1895 = str1895 + " ";
+}
+console.log(str28);
+console.log(str1895 + str28);
+console.log(str1895 + str1895 + str28);
+
+
+console.log("Exercise EASY0347 :");
+let str141 = "";
+let str241 = "";
+let e41 = 1;
+for (let i = x; i > 0; i--, e41++) {
+    str141 = str141 + i + " ";
+    str241 = str241 + e41 + " ";
+}
+str1896 = " ";
+for (let j = 0; j <= x; j++) {
+    str1896 = str1896 + " ";
+}
+console.log(str241);
+console.log(str1896 + str141);
+console.log(str1896 + str1896 + str241);
+
+
+console.log("Exercise EASY0348 :");
+let str121 = "";
+let str221 = "";
+let e21 = 1;
+for (let i = x; i > 0; i--, e21++) {
+    str121 = str121 + i + " ";
+    str221 = str221 + e21 + " ";
+}
+str1897 = " ";
+for (let j = 0; j <= x; j++) {
+    str1897 = str1897 + " ";
+}
+console.log(str121);
+console.log(str1897 + str221);
+console.log(str1897 + str1897 + str121);
+
+
+console.log("Exercise EASY0349 :");
+let str29 = "";
+for (let i = 1; i <= x; i++) {
+    str29 = str29 + i + " ";
+}
+str1898 = " ";
+for (let j = 0; j <= x; j++) {
+    str1898 = str1898 + " ";
+}
+console.log(str29);
+console.log(str1898 + str29);
+console.log(str1898 + str1898 + str29);
+console.log(str1898 + str1898 + str1898 + str29);
+
+
+console.log("Exercise EASY0350 :");
+let str1 = "";
+let str2 = "";
+let e = 1;
+for (let i = x; i > 0; i--, e++) {
+    str1 = str1 + i + " ";
+    str2 = str2 + e + " ";
+}
+str1899 = " ";
+for (let j = 0; j <= x; j++) {
+    str1899 = str1899 + " ";
+}
+console.log(str2);
+console.log(str1899 + str1);
+console.log(str1899 + str1899 + str2);
+console.log(str1899 + str1899 + str1899 + str1);
+
+
+console.log("Exercise EASY0351 :");
+let num3 = 2;
+let strin = "";
+for (let i = 0; i < x; i++) {
+    strin += num3 + " ";
+    num3 += 2;
+}
+console.log(strin);
+
+
+console.log("Exercise EASY0352 :");
+let num2 = 2;
+for (let i = 0; i < x; i++) {
+    console.log(num2);
+    num2 += 2;
+}
+console.log("Exercise EASY0353 :");
+let num5 = 1;
+let strin1 = "";
+for (let i = 0; i < x; i++) {
+    strin1 += num5 + " ";
+    num5 += 2;
+}
+console.log(strin1);
+
+
+console.log("Exercise EASY0354 :");
+let num4 = 1;
+for (let i = 0; i < x; i++) {
+    console.log(num4);
+    num4 += 2;
+}
+
+console.log("Exercise EASY0355 :");
+let num6 = 1;
+let str222 = "";
+while (num6 <= x) {
+    str222 += num6 * num6 + " ";
+    num6++;
+}
+console.log(str222);
+
+console.log("Exercise EASY0356 :");
+let num = 1;
+while (num <= x) {
+    console.log(num * num + " ");
+    num++;
+}
+
+
+console.log("Exercise EASY0357 :");
+let str77 = "";
+let i17 = 3;
+let n7 = 1;
+while (n7 <= x) {
+    str77 = str77 + Math.pow(n7, i17) + " ";
+    n7++;
+}
+console.log(str77);
+
+console.log("Exercise EASY0358 :");
+let i16 = 3;
+let n6 = 1;
+while (n6 <= x) {
+    console.log(Math.pow(n6, i16) + " ");
+    n6++;
+}
+
+
+console.log("Exercise EASY0359 :");
+let num9 = 10;
+let s9 = "";
+for (let i = x; i > 0; i--) {
+    s9 += num9 + " ";
+    num9 -= 2;
+}
+console.log(s9);
+
+console.log("Exercise EASY0360 :");
+let num1 = 10;
+for (let i = x; i > 0; i--) {
+    console.log(num1);
+    num1 -= 2;
+}
+
+
+console.log("Exercise EASY0361 :");
+let num8 = 9;
+let str99 = "";
+for (let i = x; i > 0; i--) {
+    str99 += num8 + " ";
+    num8 -= 2;
+}
+console.log(str99);
+
+
+console.log("Exercise EASY0362 :");
+let num88 = 9;
+
+for (let i = x; i > 0; i--) {
+    console.log(num88);
+    num88 -= 2;
+}
+
+
+console.log("Exercise EASY0363 :");
+let num66 = 5;
+let str2226 = "";
+while (num66 > 0) {
+    str2226 += num66 * num66 + " ";
+    num66--;
+}
+console.log(str2226);
+
+console.log("Exercise EASY0364 :");
+let num666 = 5;
+while (num666 > 0) {
+    console.log(num666 * num666 + " ");
+    num666--;
+}
+
+
+console.log("Exercise EASY0365 :");
+let i164 = 3;
+let n64 = 5;
+let s44 = "";
+while (n64 > 0) {
+    s44 += Math.pow(n64, i164) + " ";
+    n64--;
+}
+console.log(s44);
+
+
+console.log("Exercise EASY0366 :");
+let i165 = 3;
+let n65 = 5;
+
+while (n65 > 0) {
+    console.log(Math.pow(n65, i165) + " ");
+    n65--;
+}
+
+
+console.log("Exercise EASY0367 :");
+let num34 = 2;
+let num44 = 10;
+let strin4 = "";
+let strin5 = "";
+for (let i = 0; i < x; i++) {
+    strin4 += num34 + " ";
+    strin5 += num44 + " ";
+    num34 += 2;
+    num44 -= 2;
+}
+console.log(strin4 + "" + strin5);
+
+
+console.log("Exercise EASY0368 :");
+let num344 = 1;
+let num444 = 9;
+let strin44 = "";
+let strin54 = "";
+for (let i = 0; i < x; i++) {
+    strin44 += num344 + " ";
+    strin54 += num444 + " ";
+    num344 += 2;
+    num444 -= 2;
+}
+console.log(strin44 + "" + strin54);
+
+
+console.log("Exercise EASY0369 :");
+let num644 = 1;
+let num6444 = 5;
+let str22244 = "";
+let str222444 = "";
+while (num644 <= x) {
+    str22244 += num644 * num644 + " ";
+    str222444 += num6444 * num6444 + " ";
+    num644++;
+    num6444--;
+}
+console.log(str22244 + "" + str222444);
+
+
+console.log("Exercise EASY0370 :");
+let str777 = "";
+let str7777 = "";
+let i177 = 3;
+let i1777 = 3;
+let n77 = 1;
+let n777 = 5;
+while (n77 <= x) {
+    str777 = str777 + Math.pow(n77, i177) + " ";
+    str7777 = str7777 + Math.pow(n777, i1777) + " ";
+    n77++;
+    n777--;
+}
+console.log(str777 + "" + str7777);
+
+
+console.log("Exercise EASY0371 :");
+let vv = "";
+for (let i = 0; i <= x; i++) {
+    if (i % 2 === 0) {
+        vv += 1 + " ";
+    } else
+        vv += 0 + " ";
+}
+console.log(vv);
+
+console.log("Exercise EASY0372 :");
+let v1 = 0;
+for (let i = 0; i < x; i++) {
+    v1 = (i % 2) === 1 ? 0 : 1;
+    console.log(v1);
+
+}
+
+console.log("Exercise EASY0373 :");
+let v7 = 0;
+let vv7 = "";
+for (let i = 0; i < x; i++) {
+    if (v7 === 0) {
+        vv7 += 1 + " ";
+        v7 = 1;
+    } else if (v7 === 1) {
+        vv7 += 2 + " ";
+        v7 = 2;
+    } else if (v7 === 2) {
+        vv7 += 0 + " ";
+        v7 = 0;
+    }
+}
+console.log(vv7);
+
+
+console.log("Exercise EASY0374 :");
+let v77 = 0;
+
+for (let i = 0; i < x; i++) {
+    if (v77 === 0) {
+        v77 = 1;
+        console.log(v77);
+    } else if (v77 === 1) {
+        v77 = 2;
+        console.log(v77);
+    } else if (v77 === 2) {
+        v77 = 0;
+        console.log(v77);
+    }
+}
+
+
+console.log("Exercise EASY0377 :");
+let k = 1;
+let kk = "";
+for (let i = 0; i < x; i++) {
+    kk += k + " ";
+    k *= -1;
+}
+console.log(kk);
+
+console.log("Exercise EASY0378 :");
+let k2 = 1;
+for (let i = 0; i < x; i++) {
+    console.log(k2);
+    k2 *= -1;
+}
+
+
+console.log("Exercise EASY0379 :");
+let k3 = -1;
+let kk3 = "";
+for (let i = 0; i < x; i++) {
+    kk3 += k3 + " ";
+    k3 *= -1;
+}
+console.log(kk3);
+
+console.log("Exercise EASY0380 :");
+let k1 = -1;
+for (let i = 0; i < x; i++) {
+    console.log(k1);
+    k1 *= -1;
+}
+
+
+console.log("Exercise EASY0381 :");
+let ff = 1;
+for (let i = 0; i < x; i++) {
+    ff *= 2;
+}
+console.log(ff);
+
+
+console.log("Exercise EASY0382 :");
+let n11 = 1;
+let i2 = 3;
+while (n11 < x) {
+    n11++;
+}
+console.log(Math.pow(i2, n11));
+
+
+console.log("Exercise EASY0383 :");
+let n111 = 1;
+let i21 = 4;
+while (n111 < x) {
+    n111++;
+}
+console.log(Math.pow(i21, n111));
+
+
+console.log("Exercise EASY0384 :");
+let n112 = 1;
+let i22 = 4;
+while (n112 < x) {
+    n112++;
+}
+console.log(Math.pow(i22, n112));
+
+console.log("Exercise EASY0385 :");
+let f = 1;
+for (let i = 0; i <= x; i++) {
+    f *= 10;
+}
+console.log(f);
+
+console.log("Exercise EASY0386 :");
+let ff1 = 1;
+for (let i = 0; i < x; i++) {
+    ff1 *= -2;
+}
+console.log(ff1);
+
+console.log("Exercise EASY0387 :");
+let ff2 = 1;
+for (let i = 0; i < x; i++) {
+    ff2 *= -3;
+}
+console.log(ff2);
+
+
+console.log("Exercise EASY0388 :");
+let ff3 = 1;
+for (let i = 0; i < x; i++) {
+    ff3 *= -4;
+}
+console.log(ff3);
+
+
+console.log("Exercise EASY0389 :");
+let ff4 = 1;
+for (let i = 0; i < x; i++) {
+    ff4 *= -5;
+}
+console.log(ff4);
+
+console.log("Exercise EASY0390 :");
+let ff5 = 1;
+for (let i = 0; i < x; i++) {
+    ff5 *= -10;
+}
+console.log(ff5);
+
+
+console.log("Exercise EASY0391 :");
+let ff6 = -1;
+for (let i = 0; i < x; i++) {
+    ff6 *= -2;
+}
+console.log(ff6);
+
+
+console.log("Exercise EASY0392 :");
+let ff7 = -1;
+for (let i = 0; i < x; i++) {
+    ff7 *= -3;
+}
+console.log(ff7);
+
+
+console.log("Exercise EASY0393 :");
+let ff8 = -1;
+for (let i = 0; i < x; i++) {
+    ff8 *= -4;
+}
+console.log(ff8);
+
+
+console.log("Exercise EASY0394 :");
+let ff9 = -1;
+for (let i = 0; i < x; i++) {
+    ff9 *= -5;
+}
+console.log(ff9);
+
+
+console.log("Exercise EASY0395 :");
+let fff = 1;
+for (let i = 0; i <= x; i++) {
+    fff *= -10;
+}
+console.log(fff);
+
+
+console.log("Exercise EASY0396 :");
+let str88 = "";
+let ffff = 2;
+for (let i = 0; i < x; i++) {
+    str88 = str88 + ffff + " ";
+    ffff *= 2;
+}
+console.log(str88);
+
+console.log("Exercise EASY0397 :");
+let str888 = "";
+let ffff1 = 3;
+for (let i = 0; i < x; i++) {
+    str888 = str888 + ffff1 + " ";
+    ffff1 *= 3;
+}
+console.log(str888);
+
+
+console.log("Exercise EASY0398 :");
+let str881 = "";
+let ffff9 = 4;
+for (let i = 0; i < x; i++) {
+    str881 = str881 + ffff9 + " ";
+    ffff9 *= 4;
+}
+console.log(str881);
+
+
+console.log("Exercise EASY0399 :");
+let str884 = "";
+let ffff4 = 5;
+for (let i = 0; i < x; i++) {
+    str884 = str884 + ffff4 + " ";
+    ffff4 *= 5;
+}
+console.log(str884);
+
+
+console.log("Exercise EASY0400 :");
+let str87 = "";
+let ffff2 = 10;
+for (let i = 0; i < x; i++) {
+    str87 = str87 + ffff2 + " ";
+    ffff2 *= 10;
+}
+console.log(str87);
+
---
+++ Javascript/Lesson/Lesson_6/Lesson_6.js
@@ -0,0 +1,16 @@
+let n = 10;
+while (n < 10) {
+    console.log(n);
+    n++;
+}
+
+
+let a = [1,5,4,8,7,5,6,7];
+for (let i = 0; i < a.length; i++) {
+    console.log(a[i]);
+
+}
+console.log("____________");
+for (let e of a){
+    console.log(e);
+}
